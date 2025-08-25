4fb107460fa066167bd48918febc5607c8499613
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-08-25 11:22:59 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,14 @@
-export default { reactStrictMode: true };
+// next.config.mjs (ESM)
+import path from 'path';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    // Mappe "@" vers la racine du projet
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    return config;
+  },
+};
+
+export default nextConfig;
+
