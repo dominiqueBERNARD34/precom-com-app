105afeb88e8c3cfadc334dbfcf2216fa8eac94cc
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-08-26 11:00:58 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from '@/lib/supabaseClient';
+
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
