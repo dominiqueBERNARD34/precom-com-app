337c219b4e8179b0d03e7b43b0a863bf6f77c8a5
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-08-26 11:15:12 +0200

    Update page.tsx

--- app/import/page.tsx
+++ app/import/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
 import * as XLSX from 'xlsx';
-import supabase from '@/lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import Back from '@/componants/Back';
 
 type Row = Record<string, any>;
