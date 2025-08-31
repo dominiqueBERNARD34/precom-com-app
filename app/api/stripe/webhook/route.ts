a09241c957cbed7997de54282f067d600cab993c
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-08-31 15:39:45 +0200

    runtime node.js

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2024-06-20',
