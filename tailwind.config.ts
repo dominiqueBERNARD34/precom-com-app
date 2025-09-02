3a2411d5b3e13f8ebe5b81e175f04fd15d73466f
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-09-02 17:47:40 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,23 +5,13 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#00c2ff',
-          50:  '#e6f8ff',
-          100: '#ccf0ff',
-          200: '#99e1ff',
-          300: '#66d2ff',
-          400: '#33c3ff',
-          500: '#00c2ff',
-          600: '#00a8e5',
-          700: '#0089bd',
-          800: '#006c95',
-          900: '#004e6c'
-        }
-      }
-    }
+        brand: {
+          400: '#2dd4bf', // turquoise clair
+          500: '#14b8a6', // turquoise "Maestra"
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
-
 export default config;
