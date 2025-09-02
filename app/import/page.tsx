eb73adb14965d63a9a79c3a0185dd1d2837e131f
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-09-02 10:27:48 +0200

    Create page.tsx

---
+++ app/import/page.tsx
@@ -0,0 +1,14 @@
+// app/import/page.tsx
+import { Suspense } from 'react';
+import Client from './Client';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="p-6">Chargement…</div>}>
+      <Client />
+    </Suspense>
+  );
+}
