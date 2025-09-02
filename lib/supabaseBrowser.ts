a618bbd7d944d19ded2510d3c3f076a613a3a535
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-09-02 17:48:33 +0200

    Update supabaseBrowser.ts

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,7 +1,9 @@
 // lib/supabaseBrowser.ts
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
-export const supabaseBrowser = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-);
+export function supabaseBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
