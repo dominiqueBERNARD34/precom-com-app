78e166aa79981e164aa98238dae14e1d7812e11b
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-09-03 12:18:38 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,18 +1,6 @@
-// lib/supabaseClient.ts
-'use client';
+import { createClient } from '@supabase/supabase-js'
 
-import { createClient } from '@supabase/supabase-js';
-// import type { Database } from './types/supabase'; // si vous avez les types générés
-
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-    },
-  }
-);
-
-export default supabase;
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
