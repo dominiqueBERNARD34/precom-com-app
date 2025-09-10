471214b150fa3f9fa70bab4a9651793a9b14158d
dominiqueBERNARD34 <contact@dominiquebernard.com>
2025-09-10 18:21:38 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,6 @@
-// lib/stripe.ts
-import Stripe from 'stripe'
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2024-06-20',
-})
+// /lib/stripe.ts
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+  // On n'indique pas apiVersion : Stripe utilisera la version par défaut du compte.
+});
